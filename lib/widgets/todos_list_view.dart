cf52ee9947c7e7963aa95ae938d15373f163f3ec
Yaroslav Vasylenko <gloomy.699@gmail.com>
2021-10-01 19:05:22 +0300

    add color to alert dialog

--- lib/widgets/todos_list_view.dart
+++ lib/widgets/todos_list_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todoey/models/Todo.dart';
 import 'package:todoey/repository/todo_cubit.dart';
@@ -21,20 +23,30 @@ class TodosListView extends StatelessWidget {
             showDialog<String>(
               context: context,
               builder: (BuildContext _) => AlertDialog(
-                title: const Text('Alert'),
+                title: const Text(
+                  'Alert',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.red,
+                  ),
+                ),
                 content:
                     const Text('Are you sure you want to delete the task?'),
                 actions: <Widget>[
                   TextButton(
                     onPressed: () => Navigator.pop(context, 'Cancel'),
-                    child: const Text('Cancel'),
+                    child: const Text(
+                      'Cancel',
+                      style: TextStyle(color: Colors.red),
+                    ),
                   ),
                   TextButton(
                     onPressed: () {
                       BlocProvider.of<TodoCubit>(context).removeTodo(todo);
                       Navigator.pop(context);
                     },
-                    child: const Text('OK'),
+                    child:
+                        const Text('OK', style: TextStyle(color: Colors.green)),
                   ),
                 ],
               ),
